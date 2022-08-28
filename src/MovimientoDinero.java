6e6aa91deccb9c34cb91238a43bb89c89f544418
alimerag <92386043+alimerag@users.noreply.github.com>
2022-08-28 10:29:45 -0500

    clase movimientoDinerocompleta

--- src/MovimientoDinero.java
+++ src/MovimientoDinero.java
@@ -3,5 +3,35 @@ public class MovimientoDinero {
     //Atributos
     private double monto;
     private String conceptomov;
+    private String NombreUsuario;
 
+    public MovimientoDinero(double monto, String conceptomov, String NombreUsuario){
+        this.monto = monto;
+        this.conceptomov = conceptomov;
+        this.NombreUsuario = NombreUsuario;
+    }
+
+    public double getmonto() {
+        return monto;
+    }
+
+    public void setmonto(double monto) {
+        this.monto = monto;
+    }
+
+    public String getconceptoMov() {
+        return conceptomov;
+    }
+
+    public void setconceptomov(String conceptomov) {
+        this.conceptomov = conceptomov;
+    }
+
+    public String getNombreUsuario() {
+        return NombreUsuario;
+    }
+
+    public void setNombreUsuario(String NombreUsuario) {
+        this.NombreUsuario = NombreUsuario;
+    }
 }
